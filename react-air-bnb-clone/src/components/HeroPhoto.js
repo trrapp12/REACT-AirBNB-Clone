bc19deac2817ab098dcc97a26282583d06e68508
Trevor Rapp <trrapp@gmail.com>
2022-02-24 01:25:35 -0700

    updates components

--- react-air-bnb-clone/src/components/HeroPhoto.js
+++ react-air-bnb-clone/src/components/HeroPhoto.js
@@ -3,8 +3,8 @@ import React from 'react'
 function HeroPhoto (props) {
   return (
   <div className="upper--double-container">
-    <img className="upper--top-img" src={props.image1} alt="props.alt1"></img>
-    <img className="upper--bottom-img" src={props.image2} alt="props.alt2"></img>
+    <img className="upper--top-img" src={props.image1} alt={props.alt1}></img>
+    <img className="upper--bottom-img" src={props.image2} alt={props.alt2}></img>
   </div>
   )
 }
--- react-air-bnb-clone/src/components/UpperFold.js
+++ react-air-bnb-clone/src/components/UpperFold.js
@@ -13,7 +13,7 @@ import HeroPhoto from './HeroPhoto'
 
 function UpperFold () {
   return (
-    <>
+    <div className="upper--main-container">
       <div className="upper--single-photo-container">
         <img className="upper--single-photo" src={image_28} alt="photo of hands which appear to be performing a seance over top of a wok"></img>
       </div>
@@ -21,9 +21,9 @@ function UpperFold () {
       <HeroPhoto image1={image_27} image2={image_29} alt1="salt falls on food" alt2="close up of hands playing a guitar"/>
       <HeroPhoto image1={image_22} image2={image_23} alt1="closeup of hands playing a keyboard" alt2="person does butterfly stroke in pool"/>
       <HeroPhoto image1={image_25} image2={image_24} alt1="young woman meditates on a yoga mat" alt2="young person sings solo into mic"/>
-      <h1>Online Experiences</h1>
-      <p>Join unique interactive activities led by one-of-a-kind hosts &mdash;all without leaving your home.</p>
-    </>
+      <h1 className="upper--title">Online Experiences</h1>
+      <p className="upper--content">Join unique interactive activities led by one-of-a-kind hosts &mdash;all without leaving your home.</p>
+    </div>
   )
 }
 
